5edd1a12afa8186eb0b745effe7d8dd0adc97176
Alex Kim <kim2975@purdue.edu>
2025-02-04 19:44:38 -0500

    lab03

--- lab03/lab03.cpp
+++ lab03/lab03.cpp
@@ -1,20 +1,35 @@
-// lab03.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+void printMenu(int& choice) {
+	cout << "Welcome to my program, please select an operation to perform:" << endl;
+	cout << "1. Placeholder" << endl;
+	cout << "2. Another Placeholder" << endl;
+	cout << "3. And a third one" << endl;
+	cout << "\nYour Selection: ";
+	cin >> choice;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void getChoices(float& A, float& B) {
+	cout << "Please enter the first value:";
+	cin >> A;
+	// The rest of this function is an exercise to the reader
+}
+void firstChoice(float A, float B) {
+	cout << "This is proving the first choice ran" << endl;
+}
+void secondChoice(float A, float B) {
+	cout << "This is proving the second choice ran" << endl;
+}
+int main() {
+	int choice;
+	float A;
+	float B;
+	printMenu(choice);
+	getChoices(A, B);
+	if (choice == 1) {
+		firstChoice(A, B);
+	}
+	if (choice == 2) {
+		secondChoice(A, B);
+	}
+	return 0;
+}
